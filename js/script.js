3cac87dcd4b547b1484638aa7873f0156437d30e
Mehmet Dev. <akmesemehmetsahin77@gmail.com>
2026-07-15 06:31:19 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function white() {
+    document.documentElement.style.setProperty('--ana-arka-plan', '#ffffff');
+    document.documentElement.style.setProperty('--panel-arka-plan', '#f0f0f0');
+    document.documentElement.style.setProperty('--ana-yazi', '#111111');
+    document.documentElement.style.setProperty('--baslik-rengi', '#000000');
+  }
+
+  function dark() {
+    document.documentElement.style.setProperty('--ana-arka-plan', '#111');
+    document.documentElement.style.setProperty('--panel-arka-plan', '#1b1b1b');
+    document.documentElement.style.setProperty('--ana-yazi', '#eaeaea');
+    document.documentElement.style.setProperty('--baslik-rengi', '#ffffff');
+  }
+    function darkblue() {
+  document.documentElement.style.setProperty('--ana-arka-plan', 'var(--zoktay-mavisi)');
+  document.documentElement.style.setProperty('--panel-arka-plan', '#003366');
+  document.documentElement.style.setProperty('--ana-yazi', '#eaeaea');
+  document.documentElement.style.setProperty('--baslik-rengi', '#ffffff');
+    }
